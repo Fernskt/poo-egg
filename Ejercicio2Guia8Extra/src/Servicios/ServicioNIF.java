775ee2a2eec54af836af0756f9586c16ae164d5d
Fernando Hasperue <fernando.hasperue@gmail.com>
2023-04-25 17:21:58 -0300

    Ultimo ejercicio guia 8

--- Ejercicio2Guia8Extra/src/Servicios/ServicioNIF.java
+++ Ejercicio2Guia8Extra/src/Servicios/ServicioNIF.java
@@ -8,20 +8,35 @@ package Servicios;
 import Entidades.NIF;
 import java.util.Scanner;
 
-
 public class ServicioNIF {
-    
+
     Scanner leer = new Scanner(System.in);
     NIF n = new NIF();
-    public NIF crearNif(){
-        
+
+    public NIF crearNif() {
+
         System.out.println("Ingrese n de DNI");
         n.setDni(leer.nextLong());
-        
-        
-        
-        
-        
+
         return n;
+    }
+
+    public String calcular(NIF n) {
+
+        String[] vector = {"T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"};
+
+        long num = n.getDni() % 23;
+        System.out.println("Posicion: " + num);
+
+        for (int i = 0; i < vector.length; i++) {
+            if (i == num) {
+                n.setLetra(vector[i]);
+            }
+        }
+        return n.getLetra();
+    }
+
+    public void mostrar(NIF n) {
+        System.out.println(n.getDni() + "-" + calcular(n));
     }
 }
--- Ejercicio2Guia8Extra/src/ejercicio2guia8extra/Ejercicio2Guia8Extra.java
+++ Ejercicio2Guia8Extra/src/ejercicio2guia8extra/Ejercicio2Guia8Extra.java
@@ -1,11 +1,17 @@
 
 package ejercicio2guia8extra;
 
+import Entidades.NIF;
+import Servicios.ServicioNIF;
+
 
 public class Ejercicio2Guia8Extra {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+       ServicioNIF nif = new ServicioNIF();
+       NIF n = nif.crearNif();
+       nif.mostrar(n);
+       
     }
     
 }
