4c76e41c08a2141850595f49be2da176889e2f23
Fernando Hasperue <fernando.hasperue@gmail.com>
2023-05-14 20:56:40 -0300

    Guia 10 - Colecciones

---
+++ Guia10/src/guia10/Guia10.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia10;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+
+/**
+ *
+ * @author Pc
+ */
+public class Guia10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       ArrayList<Integer> listado = new ArrayList();
+        TreeSet<Integer> numeros = new TreeSet();
+        LinkedHashSet<Integer> personas = new LinkedHashSet();
+    }
+    
+}
