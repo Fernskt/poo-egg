62b5a527d9e78227b111e1c86d6998c8ed65b023
Fernando Hasperue <fernando.hasperue@gmail.com>
2023-05-19 18:55:24 -0300

    ejercicio razas de perros

--- Guia10Ejercicio1/src/guia10ejercicio1/Guia10Ejercicio1.java
+++ Guia10Ejercicio1/src/guia10ejercicio1/Guia10Ejercicio1.java
@@ -6,6 +6,9 @@
 package guia10ejercicio1;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.Scanner;
 
 /**
@@ -18,16 +21,16 @@ public class Guia10Ejercicio1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ArrayList<String> razasPerros = new ArrayList<>();
+        ArrayList<String> razasPerros = new ArrayList();
         Scanner leer = new Scanner(System.in);
         
         Boolean continuar = true;
         
         while (continuar) {
         System.out.println("Ingrese una raza de perro: ");
-        String raza = leer.nextLine();
+       
         
-        razasPerros.add(raza);
+        razasPerros.add(leer.next().toLowerCase());
         
             System.out.println("¿Desea continuar Y/N?");
             String opcion = leer.next();
@@ -37,10 +40,49 @@ public class Guia10Ejercicio1 {
             
     }
        
+        
+        
+       
         for (String razas : razasPerros) {
             System.out.println("Raza: " + razas);
         }
         
-    }
-    
+         System.out.println(razasPerros.size());
+        
+        System.out.println("Indique qué perro quiere eliminar");
+        String perro = leer.next();
+        
+        
+        
+        Iterator<String> iterator = razasPerros.iterator();
+        boolean esta = false;
+       
+        
+       
+        
+        while(iterator.hasNext()){
+            if(iterator.next().equals(perro)){
+                iterator.remove();
+                System.out.println("Perro " + perro +" eliminado"); 
+                esta = true;
+            }
+       
+        }
+        if(esta == false){
+                System.out.println("La raza no se encuentra en la lista");
+            }
+        
+        Collections.sort(razasPerros);
+        
+         for (String razas : razasPerros) {
+            System.out.println("Raza: " + razas);
+        }
+        
+        
+    } 
+//   Continuando el ejercicio anterior, después de mostrar los perros, al usuario se le pedirá
+//un perro y se recorrerá la lista con un Iterator, se buscará el perro en la lista. Si el perro
+//está en la lista, se eliminará el perro que ingresó el usuario y se mostrará la lista
+//ordenada. Si el perro no se encuentra en la lista, se le informará al usuario y se mostrará
+//la lista ordenada. 
 }
